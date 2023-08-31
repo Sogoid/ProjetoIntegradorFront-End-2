621c378813d53c056e94863f1890a5753adc8fd0
Sogoid <diogos_ribeiro@outlook.com>
2023-08-31 17:49:40 -0300

    update files

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,1 +1,17 @@
 $("#menu__lateral").load("./assets/components/asideComp.html");
+$("#footer").load("./assets/components/footerComp.html");
+
+// Código para fazer manipulação na Dom.
+// $(function () {
+//     checkScreenSize();
+// });
+// function checkScreenSize() {
+//     if (window.matchMedia("(max-width:700px)").matches) {
+//         $("#title__logo").remove();
+//     } else {
+//         $("#title__logo").show();
+//     }
+// }
+// window.addEventListener("resize", () => {
+//     checkScreenSize();
+// });
