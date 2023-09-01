36be66823fd669e252f33395d597d8c2db14ff05
DIOGO DA S RIBEIRO <116321555+Sogoid@users.noreply.github.com>
2023-09-01 00:44:01 -0300

    update

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,4 +1,5 @@
 $("#menu__lateral").load("./assets/components/asideComp.html");
+$("#menu__footer").load("./assets/components/asideFooterComp.html");
 $("#footer").load("./assets/components/footerComp.html");
 
 // Código para fazer manipulação na Dom.
