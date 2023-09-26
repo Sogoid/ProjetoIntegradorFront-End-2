bc12de155de85bfcf88996f589b91c3ddbfd29e6
Sogoid <diogos_ribeiro@outlook.com>
2023-09-26 17:53:34 -0300

    update files

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,21 +1,17 @@
+// Carrega o componente referente ao menu no rodapé para dispositivos moveis.
 $("#menu__footer").load("./assets/components/asideFooterComp.html");
+
+// Carrega o componente referente ao rodapé
 $("#footer").load("./assets/components/footerComp.html");
 
+// Carrega o componente referente a logo.
 $("#menu__lateral-logo").load("./assets/components/aisdeCompLogo.html");
+
+// Carrega o componente referente ao menu.
 $("#menu__lateral-nav").load("./assets/components/asideComp.html");
+
+// Carrega o componente referente ao menu DropDown.
 $("#menu__lateral-dropdown").load("./assets/components/asideCompDropdwon.html");
 
-// Código para fazer manipulação na Dom.
-// $(function () {
-//     checkScreenSize();
-// });
-// function checkScreenSize() {
-//     if (window.matchMedia("(max-width:700px)").matches) {
-//         $("#title__logo").remove();
-//     } else {
-//         $("#title__logo").show();
-//     }
-// }
-// window.addEventListener("resize", () => {
-//     checkScreenSize();
-// });
+
+
--- router/router.js
+++ router/router.js
@@ -1,59 +1,21 @@
-$(document).on("click", ".nav-link", function (e) {
-    // obter o ID do div do "data-target" do link
-    let id = $(this).data("target");
-    // ocultar TODOS os divs com o id
-    $(".section > div[id]").hide();
-    // mostrar apenas o div relevante com o ID alvo
-    $("#" + id).show();
+$(function () {
+  let routes = {
+    "": function () {
+      // Não carrega nada para a rota da home
+    },
+    "cadastro": function () {
+      $("#view").load("page/cadastroView.html");
+    },
+  };
+
+  $(window).on("hashchange", function () {
+    let hash = location.hash.replace("#", "");
+    if (routes.hasOwnProperty(hash)) {
+      routes[hash]();  // Chama a função correspondente ao hash atual
+    } else {
+      $("#content").html("<h1>404 Not Found</h1>");
+    }
+  });
+
+  $(window).trigger("hashchange");
 });
-
-// Exemplo de configuração
-// < !DOCTYPE html >
-//     <html>
-//         <head>
-//             <title>Navegação de página única</title>
-//             <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-//             <style>
-//         .section > div[id] {
-//                     display: none;
-//         }
-//             </style>
-//         </head>
-//         <body>
-//             <nav>
-//                 <ul>
-//                     <li><a href="#" class="nav-link" data-target="home">Home</a></li>
-//                     <li><a href="#" class="nav-link" data-target="about">Sobre</a></li>
-//                     <li><a href="#" class="nav-link" data-target="contact">Contato</a></li>
-//                 </ul>
-//             </nav>
-
-//             <div class="section">
-//                 <div id="home">
-//                     <h1>Home</h1>
-//                     <p>Esta é a página inicial.</p>
-//                 </div>
-
-//                 <div id="about">
-//                     <h1>Sobre</h1>
-//                     <p>Esta é a página sobre.</p>
-//                 </div>
-
-//                 <div id="contact">
-//                     <h1>Contato</h1>
-//                     <p>Esta é a página de contato.</p>
-//                 </div>
-//             </div>
-
-//             <script>
-//                 $(document).on('click','.nav-link',function(e){
-//             // obter o ID do div do "data-target" do link
-//             var id = $(this).data('target');
-//             // ocultar TODOS os divs com o id
-//             $('.section > div[id]').hide();
-//                 // mostrar apenas o div relevante com o ID alvo
-//                 $('#' + id).show();
-//         });
-//             </script>
-//         </body>
-//     </html>
