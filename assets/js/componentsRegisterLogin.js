945a3fde5ae7bc5f11820a70d051d4473eee0995
Sogoid <diogos_ribeiro@outlook.com>
2023-10-09 17:41:39 -0300

    update file

--- assets/js/componentsRegisterLogin.js
+++ assets/js/componentsRegisterLogin.js
@@ -1,40 +1,61 @@
-$("#frmCadastroLogin").on("submit", function (event) {
-    // Para evitar o envio do formulário utilizaremos a seguinte instrução:
-    event.preventDefault();
+let formIds = ["#frmCadastroLogin", "#frmCadastro"];
 
-
-    // Cadastro simplificado no Login
+function salvarDados() {
     let inputUserGitHub = $("#inputUserGitHub").val();
+    let inputRegisterCPF = $("#inputRegisterCPF").val();
     let inputRegisterUserLogin = $("#inputRegisterUserLogin").val();
     let inputRegisterEmailLogin = $("#inputRegisterEmailLogin").val();
+    let inputRegisterEnd = $("#inputRegisterEnd").val();
+    let inputRegisterDataNasc = $("#inputRegisterDataNasc").val();
     let inputRegisterPasswordLogin = $("#inputRegisterPasswordLogin").val();
     let inputRegisterRepeatPasswordLogin = $("#inputRegisterRepeatPasswordLogin").val();
     let registerAcceptTermLogin = $("#registerAcceptTermLogin").val();
 
-    let register = {
-        apelido: inputUserGitHub,
-        name: inputRegisterUserLogin,
-        email: inputRegisterEmailLogin,
-        password: inputRegisterPasswordLogin,
-        repeatPassword: inputRegisterRepeatPasswordLogin,
-        acceptTerm: registerAcceptTermLogin,
+    if (inputRegisterPasswordLogin === inputRegisterRepeatPasswordLogin) {
+
+        if (localStorage.getItem('register') === null ||
+            localStorage.getItem('register') === '' ||
+            localStorage.getItem('register') === undefined) {
+            let data = [];
+            localStorage.setItem('register', JSON.stringify(data));
+        }
+
+        let dados = JSON.parse(localStorage.getItem('register'));
+
+        // Cria um objeto com todas as informações, exceto repeatPassword
+        let register = {
+            apelido: inputUserGitHub,
+            cpfUser: inputRegisterCPF,
+            name: inputRegisterUserLogin,
+            dataNascimento: inputRegisterDataNasc,
+            email: inputRegisterEmailLogin,
+            endereco: inputRegisterEnd,
+            password: inputRegisterPasswordLogin,
+            acceptTerm: registerAcceptTermLogin,
+        }
+
+        dados.push(register); // Adiciona o objeto ao array
+        localStorage.setItem('register', JSON.stringify(dados)); // Salva o array no localStorage
+
+        console.log(dados);
+    } else {
+        console.log("As senhas não são iguais");
     }
+}
 
-    // Função para validar campos
-    // if (!validaCampos(register)) return;
-
-    if (localStorage.getItem('register') === null ||
-        localStorage.getItem('register') === '' || localStorage.getItem('register') === undefined) {
-        let data = [];
-        localStorage.setItem('register', JSON.stringify(data));
-    }
-
-    let dados = JSON.parse(localStorage.getItem('register'));
-    dados.push(register);
-    localStorage.setItem('register', JSON.stringify(dados));
-
-    console.log(dados);
+formIds.forEach(function (formId) {
+    $(formId).on("submit", function (event) {
+        event.preventDefault();
+        salvarDados();
+    });
 });
+
+$("#btnSalvar").on("click", function (event) {
+    event.preventDefault();
+    salvarDados();
+});
+
+
 
 // Função para limpar o localStorage do navegador.
 // $("#btn-clean").one("click", () => {
